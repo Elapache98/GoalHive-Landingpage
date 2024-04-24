5fc234ca6f6f1377e49c871fe314258cc585c9fe
Your Name <you@example.com>
2024-04-24 10:43:16 -0400

    fixedNavLogic

--- script.js
+++ script.js
@@ -3,35 +3,36 @@ document.addEventListener('DOMContentLoaded', function() {
     var navLinks = document.getElementById('nav-links');
     var conversion = document.getElementById('conversion');
 
-    // Function to handle hamburger menu click event
+// Initially show the navigation links and conversion items till window is resized.
+    navLinks.style.display = 'flex';
+conversion.style.display = 'flex';
+
+
+
+
+
+// Function to handle hamburger menu click event
     function toggleMenu() {
         // Toggle display for navLinks and conversion
-        if (navLinks.style.display === 'none' || navLinks.style.display === '') {
-            navLinks.style.display = 'flex';
-            conversion.style.display = 'flex';
-        } else {
-            navLinks.style.display = 'none';
-            conversion.style.display = 'none';
-        }
+        const shouldDisplay = navLinks.style.display === 'none' || navLinks.style.display === '';
+        navLinks.style.display = shouldDisplay ? 'flex' : 'none';
+        conversion.style.display = shouldDisplay ? 'flex' : 'none';
     }
-    // Event listener for hamburger menu click
-    hamburger.addEventListener('click', toggleMenu);
 
-    // Event listener for window resize
-    window.addEventListener('resize', function() {
-        // Reset display for navLinks and conversion when window size changes
-        if (window.innerWidth > 770) {
-            navLinks.style.display = 'flex';
-            conversion.style.display = 'flex';
-        }
-        else {
-            if (window.innerWidth < 770 || window.innerWidth === 770) {
-                navLinks.style.display = 'none';
-                conversion.style.display ='none';
-            }
-        }
-    });
+// Event listener for window resize
+window.addEventListener('resize', function() {
+    // Reset display for navLinks and conversion when window size changes
+    if (window.innerWidth > 800) {
+        navLinks.style.display = 'flex';
+        conversion.style.display = 'flex';
+    } else {
+        navLinks.style.display = 'none';
+        conversion.style.display = 'none';
+    }
+});
 
+// Event listener for hamburger menu click
+hamburger.addEventListener('click', toggleMenu);
 
 });
 
